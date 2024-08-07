ff9d02914c33240ff0559c9ae47b0d91cafba991
Jothcv <soulkry.0@gmail.com>
2024-08-07 09:58:42 -0600

    se agrego parImpar como colaboracion de Jhonatan para edgar

---
+++ ParImpar.c
@@ -0,0 +1,16 @@
+//Jhonatan
+
+#include <stdio.h>
+int main(){
+    int num;
+    printf("ingresa un numero para ver si es par o impar :");
+    scanf("%d",&num);
+    if (num%2 ==0)
+    {
+        printf("tu numero es par ");
+    }
+    else{
+        printf("tu numero es impar");
+    }
+    
+}
