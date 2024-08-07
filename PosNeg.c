39e669b0c754da3fe87bead184300ac213f8a96f
Jothcv <soulkry.0@gmail.com>
2024-08-07 11:01:17 -0600

    se agrego posNeg como colaboracion de Jhonatan para edgar

---
+++ PosNeg.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int num;
+    printf("ingresa tu numero para ver si es positivo o negativo");
+    scanf("%d",&num);
+    if (num>0)
+    {
+        printf("tu numero es positivo ");
+    }
+    else{
+        printf("tu numero es negativo");
+    }
+    
+}
